d057aac362058afe9e517f55180c12a62bce7dac
Shardul1908 <sharduld80@gmail.com>
2023-12-27 06:34:29 +0530

    Edit main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from git import Repo
+
+def scrape_git_commits():
+    leetcode_commits = dict()
+
+    for commit in Repo(os.getcwd()).iter_commits():
+        c_message = commit.message
+        if commit.message not in leetcode_commits and c_message.startswith("Leetcode"):
+            leetcode_commits[commit.message] = commit.hexsha
+
+    return leetcode_commits
+
+if __name__ == "__main__":
+    scrape_git_commits()
